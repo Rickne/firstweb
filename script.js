8c6c961d49d711791a2813f33ca41e3b00ab091e
Rickne <113757102+Rickne@users.noreply.github.com>
2022-09-19 13:49:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ form.onsubmit = (e) => {
    xhr.onload = () =>{
         if(xhr.readyState == 4 && xhr.status == 200){
             let response = xhr.response;
-            if(response.indexOf("Invalid email address!") != -1 || response.indexOf("Failed to send your message"))
+            if(response.indexOf("Invalid email address!")  || response.indexOf("Failed to send your message"))
             {
             statusTxt.style.color = "#ff4c29";
     
